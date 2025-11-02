ff3e630bf1ef5eea6016b4416fb50225bad9dc9b
blackhole-hope123 <smile-369@hotmail.com>
2025-11-02 16:53:19 -0500

    minor updates

--- utils.py
+++ utils.py
@@ -9,12 +9,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import keras_tuner as kt
 
 num_of_categories=43
 IMG_WIDTH=30
 IMG_HEIGHT=30
 
-# a method suitable for both train data and test data loading
 def load_data(data_dir, csv_file_path, img_width, img_height):
     images,labels=[],[]
     df=pd.read_csv(csv_file_path)
@@ -32,52 +32,8 @@ def load_data(data_dir, csv_file_path, img_width, img_height):
     print("Data loaded from ", csv_file_path)
     return (images,labels)
 
-# for loading the test and training data
-def load_train_and_test_data(data_dir, img_width, img_height):
-    train,test="Train.csv","Test.csv"
-    files=set([f for f in os.listdir(data_dir)])
-    if test in files:
-        test_label_path=os.path.join(data_dir, test)
-        x_test,y_test=load_data(data_dir, test_label_path, img_width, img_height)
-    else:
-        raise Exception("Test labels are not available")
-    if train in files:
-        train_label_path=os.path.join(data_dir, train)
-        x_train,y_train=load_data(data_dir, train_label_path, img_width, img_height)
-    else:
-        raise Exception("Training labels are not available")
-    return (x_train,y_train,x_test,y_test)
-
-
-'''
-Another way of importing the training data
-'''
-'''
-def load_training_data(data_dir):
-    x_train, y_train = [], []
-    folders = [f for f in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, f))]
-    for i in range(len(folders)):
-        label = int(folders[i])
-        new_data_dir = os.path.join(data_dir, folders[i])
-        files = [f for f in os.listdir(new_data_dir) if os.path.isfile(os.path.join(new_data_dir, f))]
-        for j in range(len(files)):
-            file_path = os.path.join(new_data_dir, files[j])
-            image = cv2.imread(file_path)
-            resized_image = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT))
-            x_train.append(resized_image)
-            y_train.append(label)
-    return (x_train, y_train)
-'''
-
-def data_preprocessing(x_train,x_test):
-    # mean subtraction and normalization
-    mean = np.mean(x_train, axis=0)
-    std = np.std(x_train, axis=0)
-    x_train_processed = (x_train - mean) / std
-    # we subtract the mean of the training data to prevent the model from accessing information from the test data, ensuring a precise evaluation.
-    x_test_processed = (x_test - mean) / std
-    print("Data preprocessing completed.")
-    return (x_train_processed, x_test_processed)
+def save_npz(filename, X, y):
+    np.savez_compressed(filename, X=X, y=y)
 
 
 def get_model(regularizer_strength,dropout_rate, batch_normalization, img_width, img_height):
@@ -98,12 +54,12 @@ def get_model(regularizer_strength,dropout_rate, batch_normalization, img_width,
         loss="categorical_crossentropy",
         metrics=["accuracy"]
     )
-    print("Model trained.")
+    print("Model constructed.")
     return model
 
 
 # draw the heatmap for overfitting or underfitting
-def draw_heatmap(datapoints,over_or_under):
+def draw_heatmap(datapoints,title):
 
     datapoints = np.array(datapoints)
 
@@ -120,8 +76,5 @@ def draw_heatmap(datapoints,over_or_under):
     plt.colorbar(label='accu(drop,reg)')
     plt.xlabel('dropout_rate')
     plt.ylabel('regularizer_strength')
-    if over_or_under:
-        plt.title('overfitting')
-    else:
-        plt.title('underfitting')
+    plt.title(title)
     plt.show()
